1f6e57a0c799d0b9433f3e3babd0b1053c7a8534
Piotr <pio.ignacy@gmail.com>
2023-03-22 20:07:09 +0100

    Add key generation

--- ioproject/ioproject/settings.py
+++ ioproject/ioproject/settings.py
@@ -15,18 +15,25 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)uue0hj$%)=pfyhxqzz6==u4jj^_xww&epl_(4m9*8i#-v=*+2'
+import os
+import codecs
+
+if os.path.exists(f"{BASE_DIR}/ioproject/secret.key"):
+    with open(f"{BASE_DIR}/ioproject/secret.key", 'r') as f:
+        SECRET_KEY = f.read()
+else:
+    SECRET_KEY = codecs.encode(os.urandom(64), "hex").decode("ascii")
+    with open(f"{BASE_DIR}/ioproject/secret.key", 'w') as f:
+        f.write(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ioproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -79,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -99,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -110,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
