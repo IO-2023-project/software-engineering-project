06dfe9919b97a9d6aed331f95dae300c67a84835
bogumilap <bogumila.papiernik@gmail.com>
2023-04-27 23:52:11 +0200

    Allow adding one order at a time

--- ioproject/orders/urls.py
+++ ioproject/orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:id>', views.get_order)
+    path('<int:id>', views.get_order),
+    path('<int:id>/add_offer', views.add_offer, name="add_offer")
 ]
--- ioproject/orders/views.py
+++ ioproject/orders/views.py
@@ -1,13 +1,38 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
-from offers.models import ClientOrder
+from offers.models import ClientOrder, MechanicOffer
+from offers.status import OrderStatus
 
 
 def get_order(request, id: int):
     if request.method == "GET":
         try:
-            order = ClientOrder.objects.get(id=id)   # todo catch errors
-            return render(request, "order_details.html", {"success": True, "order": order})
+            order = ClientOrder.objects.get(id=id)
+            offers = MechanicOffer.objects.filter(client_order_id=id).values()
+            return render(request, "order_details.html", {"success": True,
+                                                          "order": order,
+                                                          "offers": offers})
         except ObjectDoesNotExist:
-            return render(request, "order_details.html", {"success": False, "id": id})
+            return render(request, "order_details.html", {"success": False,
+                                                          "message": f"Zlecenie o numerze {id} nie istnieje."})
+    return render(request, "order_details.html", {"success": False,
+                                                  "message": ""})
+
+
+def add_offer(request, id: int):
+    if request.method == "POST":
+        data = request.POST
+        content = data.get("offer_content")
+        price = data.get("offer_price")
+        order = ClientOrder.objects.filter(id=id).values()[0]
+        order.update(status=OrderStatus.WAITING_FOR_CLIENT_DECISION.value)
+        offer = MechanicOffer(client_order=ClientOrder.objects.get(id=id), offer_content=content, offer_price=price)
+        offer.save()
+        offers = MechanicOffer.objects.filter(client_order_id=id).values()
+        return render(request, "order_details.html", {"success": True,
+                                                      "order": order,
+                                                      "offers": offers})
+    return render(request, "order_details.html", {"success": False,
+                                                  "message": ""})
+
