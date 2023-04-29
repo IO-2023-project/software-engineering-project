9210f01b8425b4ca2aa6470dc8c5ab68eb517043
bogumilap <bogumila.papiernik@gmail.com>
2023-04-29 15:41:09 +0200

    Enable adding multiple offers

--- ioproject/orders/views.py
+++ ioproject/orders/views.py
@@ -1,8 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from offers.models import ClientOrder, MechanicOffer
-from offers.status import OrderStatus
 
 
 def get_order(request, id: int):
@@ -23,16 +22,12 @@ def get_order(request, id: int):
 def add_offer(request, id: int):
     if request.method == "POST":
         data = request.POST
-        content = data.get("offer_content")
-        price = data.get("offer_price")
-        order = ClientOrder.objects.filter(id=id).values()[0]
-        order.update(status=OrderStatus.WAITING_FOR_CLIENT_DECISION.value)
-        offer = MechanicOffer(client_order=ClientOrder.objects.get(id=id), offer_content=content, offer_price=price)
-        offer.save()
-        offers = MechanicOffer.objects.filter(client_order_id=id).values()
-        return render(request, "order_details.html", {"success": True,
-                                                      "order": order,
-                                                      "offers": offers})
+        for i in range(len(data) // 2):
+            content = data.get(f"offer_content_{i+1}")
+            price = data.get(f"offer_price_{i+1}")
+            offer = MechanicOffer(client_order=ClientOrder.objects.get(id=id), offer_content=content, offer_price=price)
+            offer.save()
+        return redirect(f"/orders/{id}")
     return render(request, "order_details.html", {"success": False,
                                                   "message": ""})
 
