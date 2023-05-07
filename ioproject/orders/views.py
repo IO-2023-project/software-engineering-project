97f84802917a8d0c17446322ebf6c785f3e44820
my-alaska <piotrztych@wp.pl>
2023-05-07 16:50:34 +0200

    offer display, offer link in email

--- ioproject/orders/views.py
+++ ioproject/orders/views.py
@@ -31,3 +31,16 @@ def add_offer(request, id: int):
     return render(request, "order_details.html", {"success": False,
                                                   "message": ""})
 
+def view_offer(request, id: int):
+    if request.method == "GET":
+        try:
+            order = ClientOrder.objects.get(id=id)
+            offers = MechanicOffer.objects.filter(client_order_id=id).values()
+            return render(request, "offer_contents.html", {"success": True,
+                                                          "order": order,
+                                                          "offers": offers})
+        except ObjectDoesNotExist:
+            return render(request, "offer_contents.html", {"success": False,
+                                                          "message": f"Zlecenie o numerze {id} nie istnieje."})
+    return render(request, "offer_contents.html", {"success": False,
+                                                  "message": ""})
