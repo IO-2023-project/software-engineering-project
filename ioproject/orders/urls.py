740566e71283cc9afadbcd030d2199564e0b7e07
bogumilap <bogumila.papiernik@gmail.com>
2023-05-09 11:42:01 +0200

    enable changing order's status

--- ioproject/orders/urls.py
+++ ioproject/orders/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.get_order),
     path('<int:id>/add_offer', views.add_offer, name="add_offer"),
-    path('<int:id>/view_offer', views.view_offer, name="view_offer")
+    path('<int:id>/view_offer', views.view_offer, name="view_offer"),
+    path('<int:id>/change_status/', views.change_status, name="change_status")
 ]
--- ioproject/orders/views.py
+++ ioproject/orders/views.py
@@ -2,7 +2,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponseNotAllowed
 
-from offers.models import ClientOrder, MechanicOffer
+from offers.models import ClientOrder, MechanicOffer, OrderStatus
+
+
+STATUS_LIST = [status for status in OrderStatus]
 
 
 def get_order(request, id: int):
@@ -13,7 +16,8 @@ def get_order(request, id: int):
             return render(request, "order_details.html", {"success": True,
                                                           "editable": True,
                                                           "order": order,
-                                                          "offers": offers})
+                                                          "offers": offers,
+                                                          "status_list": STATUS_LIST})
         except ObjectDoesNotExist:
             return render(request, "order_details.html", {"success": False,
                                                           "message": f"Zlecenie o numerze {id} nie istnieje."})
@@ -33,6 +37,7 @@ def add_offer(request, id: int):
     return render(request, "order_details.html", {"success": False,
                                                   "message": ""})
 
+
 def view_offer(request, id: int):
     if request.method == "GET":
         try:
@@ -41,7 +46,8 @@ def view_offer(request, id: int):
             return render(request, "order_details.html", {"success": True,
                                                           "editable": False,
                                                           "order": order,
-                                                          "offers": offers})
+                                                          "offers": offers,
+                                                          "status_list": STATUS_LIST})
         except ObjectDoesNotExist:
             raise Http404()
             # return render(request, "offer_contents.html", {"success": False,
@@ -49,3 +55,18 @@ def view_offer(request, id: int):
     return HttpResponseNotAllowed(["GET"],)
     # return render(request, "offer_contents.html", {"success": False,
     #                                               "message": ""})
+
+
+def change_status(request, id: int):
+    if request.method == "POST":
+        data = request.POST
+        try:
+            new_status = data.get("status")
+            order = ClientOrder.objects.get(id=id)
+            order.status = int(new_status)
+            order.save()
+            return redirect(f"/orders/{id}")
+        except ObjectDoesNotExist:
+            return render(request, "order_details.html", {"success": False,
+                                                          "message": f"Zlecenie o numerze {id} nie istnieje."})
+
