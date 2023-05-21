f76d0936dd4cebcea0e34c0c35811a906d131c82
my-alaska <piotrztych@wp.pl>
2023-05-21 12:09:41 +0200

    add offer choice and change status on choice

--- ioproject/orders/urls.py
+++ ioproject/orders/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('<int:id>', views.get_order),
     path('<int:id>/add_offer', views.add_offer, name="add_offer"),
     path('<int:id>/view_offer', views.view_offer, name="view_offer"),
+    path('<int:order_id>/choose_offer/<int:offer_id>', views.choose_offer, name="choose_offer"),
     path('<int:id>/change_status/', views.change_status, name="change_status")
 ]
