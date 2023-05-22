cd53d80090c112bc65d74801ff0dc8921728c931
Piotr <pio.ignacy@gmail.com>
2023-05-22 10:27:21 +0200

    Modified mechanic offer interface

--- ioproject/orders/views.py
+++ ioproject/orders/views.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponseNotAllowed
 
-from offers.models import ClientOrder, MechanicOffer, OrderStatus
+from offers.models import ClientOrder, MechanicOffer, OrderStatus, OfferItem
 
 
 STATUS_LIST = [status for status in OrderStatus]
@@ -13,10 +13,19 @@ def get_order(request, id: int):
         try:
             order = ClientOrder.objects.get(id=id)
             offers = MechanicOffer.objects.filter(client_order_id=id).values()
+            ids = [offer["id"] for offer in offers]
+            items = OfferItem.objects.filter(mechanicoffer__id__in=ids)
+            sorted_items = {}
+            for item in items:
+                mechanic_offers = MechanicOffer.objects.filter(offer_items__id=item.id)
+                for mechanic_offer in mechanic_offers:
+                    sorted_items[mechanic_offer.id] = sorted_items.get(mechanic_offer.id, []) + [item]
+
             return render(request, "order_details.html", {"success": True,
                                                           "editable": True,
                                                           "order": order,
                                                           "offers": offers,
+                                                          "order_items": sorted_items,
                                                           "status_list": STATUS_LIST})
         except ObjectDoesNotExist:
             return render(request, "order_details.html", {"success": False,
